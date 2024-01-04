5ce995ca79087e7ab3d4af4b042aaca465206fee
kursat <86690946+kursathalat@users.noreply.github.com>
2024-01-04 12:12:46 +0100

    updated callback

--- haystack_argilla/base.py
+++ haystack_argilla/base.py
@@ -2,15 +2,22 @@
 
 import warnings
 from typing import Any, Dict, List, Optional
+import os
 
 from haystack.agents import Agent
 from haystack.agents.agent_step import AgentStep
+from packaging.version import parse
 
 print("Importing base")
 
 class ArgillaCallback():
     """base"""
 
+    DEFAULT_API_URL: str = "http://localhost:6900"
+    DEFAULT_API_KEY: str = "argilla.apikey"
+
+    REPO_URL: str = "https://github.com/argilla-io/argilla"
+    ISSUES_URL: str = f"{REPO_URL}/issues"
 
     def __init__(
             self,
@@ -21,30 +28,89 @@ class ArgillaCallback():
             api_key: Optional[str] = None,
             ) -> None:
         
+        # Add event handlers to Agent
         agent.callback_manager.on_agent_start += self.on_agent_start
         agent.callback_manager.on_agent_final_answer += self.on_agent_final_answer
         agent.callback_manager.on_agent_finish += self.on_agent_finish
 
-        # Import Argilla modules
+        # Import Argilla 
         try:
             import argilla as rg
-        except:
-            raise ImportError("Please install argilla with: pip install argilla")
-            ## TODO: Add other warnings/errors
+            self.ARGILLA_VERSION = rg.__version__
+
+        except ImportError:
+            raise ImportError(
+                "To use the Argilla callback manager you need to have the `argilla` "
+                "Python package installed. Please install it with `pip install argilla`"
+            )
+
+        ## Check whether the Argilla version is compatible
+        if parse(self.ARGILLA_VERSION) < parse("1.20.0"):
+            raise ImportError(
+                f"The installed `argilla` version is {self.ARGILLA_VERSION} but "
+                "`ArgillaCallbackHandler` requires at least version 1.21.0. Please "
+                "upgrade `argilla` with `pip install --upgrade argilla`."
+            )
         
-        ## TODO: Add checks for SDK and Server versions
-        ## TODO: Add checks to confirm ApiKey and ApiUrl are valid
-        
-        # Connect to Argilla with the given credentials
+        # API_URL and API_KEY
+        # Show a warning message if Argilla will assume the default values will be used
+        if api_url is None and os.getenv("ARGILLA_API_URL") is None:
+            warnings.warn(
+                (
+                    "Since `api_url` is None, and the env var `ARGILLA_API_URL` is not"
+                    f" set, it will default to `{self.DEFAULT_API_URL}`, which is the"
+                    " default API URL in Argilla Quickstart."
+                ),
+            )
+            api_url = self.DEFAULT_API_URL
+
+        if api_key is None and os.getenv("ARGILLA_API_KEY") is None:
+            warnings.warn(
+                (
+                    "Since `api_key` is None, and the env var `ARGILLA_API_KEY` is not"
+                    f" set, it will default to `{self.DEFAULT_API_KEY}`, which is the"
+                    " default API key in Argilla Quickstart."
+                ),
+            )
+            api_key = self.DEFAULT_API_KEY
+
+        ## TODO: Add checks for Server versions
+    
+        # Connect to Argilla with the provided credentials, if applicable
         try:
             rg.init(
-                api_key=api_key,
-                api_url=api_url,
+                api_key=api_key, 
+                api_url=api_url
             )
-        except:
-            raise ValueError("Please provide valid credentials for Argilla")
+        except Exception as e:
+            raise ConnectionError(
+                f"Could not connect to Argilla with exception: '{e}'.\n"
+                "Please check your `api_key` and `api_url`, and make sure that "
+                "the Argilla server is up and running. If the problem persists "
+                f"please report it to {self.ISSUES_URL} as an `integration` issue."
+            ) from e
         
-        # Get dataset
+        # Set the Argilla variables
+        self.dataset_name = dataset_name
+        self.workspace_name = workspace_name or rg.get_workspace()
+        
+        # Retrieve the `FeedbackDataset` from Argilla
+        try:
+            self.dataset = rg.FeedbackDataset.from_argilla(
+                name=self.dataset_name,
+                workspace=self.workspace_name,
+            )
+            ## TODO: Should it be with or without records (as in Langchain integration)?
+        except Exception as e:
+            raise FileNotFoundError(
+                f"`FeedbackDataset` retrieval from Argilla failed with exception `{e}`."
+                f"\nPlease check that the dataset with name={self.dataset_name} in the"
+                f" workspace={self.workspace_name} exists in advance. If the problem persists"
+                f" please report it to {self.ISSUES_URL} as an `integration` issue."
+            ) from e
+        
+        self.field_names = [field.name for field in self.dataset.fields]
+
 
         ## TODO: Add checks to confirm dataset exists
         ## TODO: Add checks to confirm the question types are appropriate for the task
@@ -53,13 +119,6 @@ class ArgillaCallback():
         ## TODO: Add checks if records have responses and/or are submitted
         ## TODO: Add checks if there are suggestions
         ## TODO: Add checks for workspace validity
-
-        self.dataset = rg.FeedbackDataset.from_argilla(
-            name=dataset_name,
-            workspace=workspace_name,
-        )
-
-        #TODO: get corresponding field and question names from dataset
 
     def on_agent_start(self, name: str, query: str, params: Dict[str, Any]):
         pass
@@ -78,13 +137,12 @@ class ArgillaCallback():
         records=[
             {
                 "fields": {
-                    "query": query, 
-                    "retrieved_document_1": answer
+                    self.field_names[0]: query, 
+                    self.field_names[1]: answer
                     },
                 }
             ]
         )
-        return query, answer
 
     def on_tool_start(self):
         pass
