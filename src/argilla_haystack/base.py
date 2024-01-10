c2aeb0339eaaef71e4dbc3dfe95c164704d0fa8f
davidberenstein1957 <david.m.berenstein@gmail.com>
2024-01-10 18:16:17 +0100

    chore: added further config

--- src/argilla_haystack/base.py
+++ src/argilla_haystack/base.py
@@ -1,11 +1,25 @@
-#TODO: add __init__.py file
-#TODO: Review requirements.txt
-import warnings
-from typing import Any, Dict, List, Optional
-import os
-from argilla._constants import DEFAULT_API_KEY, DEFAULT_API_URL
-import logging
+# Copyright 2023-present, Argilla, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+# TODO: add __init__.py file
+# TODO: Review requirements.txt
+import logging
+import os
+import warnings
+from typing import Any, Dict, Optional
+
+from argilla._constants import DEFAULT_API_KEY, DEFAULT_API_URL
 from haystack.agents import Agent, Tool
 from haystack.agents.agent_step import AgentStep
 from packaging.version import parse
@@ -13,7 +27,8 @@ from packaging.version import parse
 _LOGGER = logging.getLogger(__name__)
 _LOGGER.setLevel(logging.INFO)
 
-class ArgillaCallback():
+
+class ArgillaCallback:
     """Callback manager that logs into Argilla
 
     Args:
@@ -31,7 +46,7 @@ class ArgillaCallback():
         ImportError: If the `argilla` Python package is not installed or the one installed is not compatible
         ConnectionError: If the connection to Argilla fails
         FileNotFoundError: If the retrieval and creation of the `FeedbackDataset` fails
-    
+
     Examples:                                                 #TODO: Correct this example if needed
         >>> from argilla_haystack.base import ArgillaCallback
         >>> from haystack.agents.base import Agent, ToolsManager
@@ -78,13 +93,13 @@ class ArgillaCallback():
             ConnectionError: If the connection to Argilla fails
             FileNotFoundError: If the retrieval and creation of the `FeedbackDataset` fails
         """
-        
+
         # Add event handlers to Agent and ToolsManager
         agent.callback_manager.on_agent_start += self.on_agent_start
         agent.callback_manager.on_agent_step += self.on_agent_step
         agent.callback_manager.on_agent_final_answer += self.on_agent_final_answer
         agent.callback_manager.on_agent_finish += self.on_agent_finish
-        
+
         agent.tm.callback_manager.on_tool_start += self.on_tool_start
         agent.tm.callback_manager.on_tool_finish += self.on_tool_finish
         agent.tm.callback_manager.on_tool_error += self.on_tool_error
@@ -92,16 +107,17 @@ class ArgillaCallback():
         # Get tool names from ToolsManager
         self.tool_names = agent.tm.get_tool_names().split(", ")
 
-        # Import Argilla 
+        # Import Argilla
         try:
             import argilla as rg
+
             self.ARGILLA_VERSION = rg.__version__
 
-        except ImportError:
+        except ImportError as e:
             raise ImportError(
                 "To use the Argilla callback manager you need to have the `argilla` "
                 "Python package installed. Please install it with `pip install argilla`"
-            )
+            ) from e
 
         ## Check whether the Argilla version is compatible
         if parse(self.ARGILLA_VERSION) < parse("1.18.0"):
@@ -110,7 +126,7 @@ class ArgillaCallback():
                 "`ArgillaCallbackHandler` requires at least version 1.18.0. Please "
                 "upgrade `argilla` with `pip install --upgrade argilla`."
             )
-        
+
         # API_URL and API_KEY
         # Show a warning message if Argilla will assume the default values will be used
         if api_url is None and os.getenv("ARGILLA_API_URL") is None:
@@ -120,6 +136,7 @@ class ArgillaCallback():
                     f" set, it will default to `{DEFAULT_API_URL}`, which is the"
                     " default API URL in Argilla Quickstart."
                 ),
+                stacklevel=2,
             )
             api_url = DEFAULT_API_URL
 
@@ -130,15 +147,13 @@ class ArgillaCallback():
                     f" set, it will default to `{DEFAULT_API_KEY}`, which is the"
                     " default API key in Argilla Quickstart."
                 ),
+                stacklevel=2,
             )
             api_key = DEFAULT_API_KEY
-    
+
         # Connect to Argilla with the provided credentials, if applicable
         try:
-            rg.init(
-                api_key=api_key, 
-                api_url=api_url
-            )
+            rg.init(api_key=api_key, api_url=api_url)
         except Exception as e:
             raise ConnectionError(
                 f"Could not connect to Argilla with exception: '{e}'.\n"
@@ -146,7 +161,7 @@ class ArgillaCallback():
                 "the Argilla server is up and running. If the problem persists "
                 f"please report it to {self.ISSUES_URL} as an `integration` issue."
             ) from e
-        
+
         # Set the Argilla variables
         self.dataset_name = dataset_name
         self.workspace_name = workspace_name or rg.get_workspace()
@@ -180,23 +195,25 @@ class ArgillaCallback():
                             required=False,
                         ),
                     ],
-                        guidelines="You're asked to rate the quality of the response and provide feedback.",
-                        allow_extra_metadata=True,
+                    guidelines="You're asked to rate the quality of the response and provide feedback.",
+                    allow_extra_metadata=True,
                 )
-                dataset.add_metadata_property(rg.TermsMetadataProperty(
-                                                    name="tool_name",
-                                                    title="Tool Name",
-                                                    values=self.tool_names))
+                dataset.add_metadata_property(
+                    rg.TermsMetadataProperty(
+                        name="tool_name", title="Tool Name", values=self.tool_names
+                    )
+                )
                 self.dataset = dataset.push_to_argilla(self.dataset_name)
                 warnings.warn(
-                (
-                    f"No dataset with the name {self.dataset_name} was found in workspace "
-                    f"{self.workspace_name}. A new dataset with the name {self.dataset_name} "
-                    "has been created with the question fields `prompt` and `response`"
-                    "and the rating question `response-rating` with values 1-5 and text question"
-                    "named `response-feedback`."
-                ),
-            )
+                    (
+                        f"No dataset with the name {self.dataset_name} was found in workspace "
+                        f"{self.workspace_name}. A new dataset with the name {self.dataset_name} "
+                        "has been created with the question fields `prompt` and `response`"
+                        "and the rating question `response-rating` with values 1-5 and text question"
+                        "named `response-feedback`."
+                    ),
+                    stacklevel=2,
+                )
 
         except Exception as e:
             raise FileNotFoundError(
@@ -204,7 +221,7 @@ class ArgillaCallback():
                 f" If the problem persists please report it to {self.ISSUES_URL} "
                 f"as an `integration` issue."
             ) from e
-        
+
         supported_fields = ["prompt", "response"]
         if supported_fields != [field.name for field in self.dataset.fields]:
             raise ValueError(
@@ -213,21 +230,23 @@ class ArgillaCallback():
                 f"`haystack` integration. Supported fields are: {supported_fields},"
                 f" and the current `FeedbackDataset` fields are {[field.name for field in self.dataset.fields]}."
             )
-        
+
         self.field_names = [field.name for field in self.dataset.fields]
         self.metadata = {}
 
-    def on_agent_start(self, name: str, query: str, params: Dict[str, Any], **kwargs: Any) -> None:
+    def on_agent_start(
+        self, name: str, query: str, params: Dict[str, Any], **kwargs: Any
+    ) -> None:
         """Do nothing when the agent starts"""
         pass
 
     def on_agent_step(self, agent_step: AgentStep, **kwargs: Any) -> None:
         """Update the metadata for the record with the tool output at agent step"""
         self.metadata["tool_output"] = agent_step.prompt_node_response
-    
+
     def on_agent_finish(self, agent_step: AgentStep, **kwargs: Any) -> None:
         """Do nothing when the agent finishes"""
-    
+
     def on_agent_final_answer(self, final_answer, **kwargs: Any) -> None:
         """Add the final answer and the query to the record and submit it to Argilla"""
         query = final_answer["query"]
@@ -235,21 +254,24 @@ class ArgillaCallback():
         self.dataset.add_records(
             records=[
                 {
-                    "fields": {
-                        "prompt": query, 
-                        "response": answer
-                        },
-                    "metadata": self.metadata
-                    },
-                ]
-            )
+                    "fields": {"prompt": query, "response": answer},
+                    "metadata": self.metadata,
+                },
+            ]
+        )
         _LOGGER.info("Records have been updated to Argilla")
 
     def on_tool_start(self, tool_input: str, tool: Tool):
         """Do nothing when the tool starts"""
         pass
 
-    def on_tool_finish(self, tool_result: str, tool_name: Optional[str] = None, tool_input: Optional[str] = None, **kwargs: Any) -> None:
+    def on_tool_finish(
+        self,
+        tool_result: str,
+        tool_name: Optional[str] = None,
+        tool_input: Optional[str] = None,
+        **kwargs: Any,
+    ) -> None:
         """Update the metadata with the tool name"""
         self.metadata["tool_name"] = tool_name
 
